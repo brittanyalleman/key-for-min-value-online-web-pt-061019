792fed78fe313256fec6f49ab90a476e4a7a5eb6
Brittany Alleman <brittany.alleman1@gmail.com>
2019-06-25 00:23:11 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  name = nil
+  min_value = nil
+  name_hash.each do |key, value|
 
+  end
 end
