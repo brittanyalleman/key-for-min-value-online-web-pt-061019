95f4efbb06452fed220361007a352688dbbb0c0d
Brittany Alleman <brittany.alleman1@gmail.com>
2019-06-25 00:41:43 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,10 +4,10 @@
 def key_for_min_value(name_hash)
 name = nil
 min_value = nil
-name_hash.each do |key, value|
+ name_hash.each do |key, value|
    if value < min_value
     name = key
     min_value = value
   end
-end
+ end
 end
